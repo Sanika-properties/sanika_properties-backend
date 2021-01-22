1e47ecf64d9c3d5defa323c956da9a948d8ad9b1
arvind <arvind.bisht.90475@gmail.com>
2021-01-22 17:10:25 +1000

    changed db url

--- src/db.config.js
+++ src/db.config.js
@@ -1,22 +1,18 @@
-require('dotenv').config();
-const app = require('express')();
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/sanika-properties' || process.env.DBURL;
-
+require("dotenv").config();
+const app = require("express")();
+const mongoose = require("mongoose");
+const url = process.env.DBURL || "mongodb://localhost:27017/sanika-properties";
 
 module.exports = async function connectDb() {
-    try {
-        await mongoose.connect(url,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-                useFindAndModify: false
-            }
-        );
-        console.log('connected to DB');
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("connected to DB");
+  } catch (error) {
+    console.log(error.message);
+  }
+};

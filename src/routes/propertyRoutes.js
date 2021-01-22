710ecbbee2c3d777c191bfc7f6b22429efe48515
arvind <arvind.bisht.90475@gmail.com>
2021-01-22 14:21:40 +1000

    removed bug from deployment

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,34 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAll, getOne, create, remove, update, search } = require('./../controllers/propertyControllers');
-const { validateProperty } = require('../validations')
-<<<<<<< HEAD
-const { isLoggedIn, isAdmin } = require('../middlewares/index');
-=======
-const { isLoggedIn, isAdmin, searchAndFilterProperty } = require('../middlewares/index');
->>>>>>> develop
-const multer = require('multer');
-const { storage } = require('../../cloudinary');
+const {
+  getAll,
+  getOne,
+  create,
+  remove,
+  update,
+  search,
+} = require("./../controllers/propertyControllers");
+const { validateProperty } = require("../validations");
+const {
+  isLoggedIn,
+  isAdmin,
+  searchAndFilterProperty,
+} = require("../middlewares/index");
+const multer = require("multer");
+const { storage } = require("../../cloudinary");
 const upload = multer({ storage });
 
+router.get("/", searchAndFilterProperty, getAll);
 
-router.get('/', searchAndFilterProperty, getAll);
+router.get("/:id", getOne);
 
-router.get('/:id', getOne);
+router.post(
+  "/",
+  isLoggedIn,
+  isAdmin,
+  upload.array("image"),
+  validateProperty,
+  create
+);
 
-router.post('/', isLoggedIn, isAdmin, upload.array('image'), validateProperty, create);
-<<<<<<< HEAD
+router.put(
+  "/:id",
+  isLoggedIn,
+  isAdmin,
+  upload.array("image"),
+  validateProperty,
+  update
+);
 
-router.put('/:id', isLoggedIn, isAdmin, upload.array('image'), validateProperty, update);
-
-router.delete('/:id', isLoggedIn, isAdmin, remove);
-=======
-
-router.put('/:id', isLoggedIn, isAdmin, upload.array('image'), validateProperty, update);
-
-router.delete('/:id', isLoggedIn, isAdmin, remove);
-
-
->>>>>>> develop
+router.delete("/:id", isLoggedIn, isAdmin, remove);
 
 module.exports = router;
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,62 +1,50 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const user = Joi.object({
-    email: Joi.string().required()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    password: Joi.string().min(6).max(30).required()
-
+  email: Joi.string()
+    .required()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+  password: Joi.string().min(6).max(30).required(),
 });
 
-
 const property = Joi.object({
-    title: Joi.string().required(),
-<<<<<<< HEAD
-    // imageUrl: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required()
-=======
-    description: Joi.string().required(),
-    purpose: Joi.string().allow('Buy', 'Sell').required(),
-    price: Joi.number().required(),
-    location: Joi.string().required(),
-    images: Joi.object(),
->>>>>>> develop
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  purpose: Joi.string().allow("Buy", "Sell").required(),
+  price: Joi.number().required(),
+  location: Joi.string().required(),
+  images: Joi.object(),
 });
 
 const validateUser = (req, res, next) => {
-    try {
-        const { error } = user.validate(req.body);
-        if (error) {
-            res.status(400);
-            throw new Error(error);
-        } else {
-            next();
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const { error } = user.validate(req.body);
+    if (error) {
+      res.status(400);
+      throw new Error(error);
+    } else {
+      next();
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 const validateProperty = (req, res, next) => {
-    try {
-        const { error } = property.validate(req.body);
-        if (error) {
-            res.status(400);
-            throw new Error(error);
-        } else {
-            next();
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const { error } = property.validate(req.body);
+    if (error) {
+      res.status(400);
+      throw new Error(error);
+    } else {
+      next();
     }
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    validateUser,
-    validateProperty
-}
-
-
-
-
+  validateUser,
+  validateProperty,
+};
